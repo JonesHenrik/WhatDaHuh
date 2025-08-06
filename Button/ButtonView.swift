7a6f082f7e7c2b84564338d253ca96f1e6f0ff4e
Henrik Jones <henrikjones22@gmail.com>
2025-08-06 11:43:40 -0400

    polished buttons with needed items

--- Button/ButtonView.swift
+++ Button/ButtonView.swift
@@ -11,16 +11,17 @@ struct ButtonView: View {
     var sfSymbol: String
     
     var body: some View {
-        GeometryReader { geometry in
+
             ZStack {
                 Circle()
                     .foregroundStyle(.buttonBackground)
-                    .frame(width: geometry.size.width * 0.33)
+                    
                 Image(systemName: sfSymbol)
                     .foregroundStyle(.white)
-                    .frame(width: geometry.size.width * 0.67)
+                    .padding()
             }
-        }
+            .padding(.top)
+        
     }
 }
 
