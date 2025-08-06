fa50537a25cecf66967c8db54a8170cf97c40501
Maria Reyna <maria.reyna2244@gmail.com>
2025-08-06 16:39:50 -0400

    added badges fixed organization with assets

--- WhatDaHuh/BadgeGrid/BadgeGridView.swift
+++ WhatDaHuh/BadgeGrid/BadgeGridView.swift
@@ -8,18 +8,41 @@
 import SwiftUI
 
 struct BadgeGridView: View {
+    @Environment(Router.self) var router
+    
+    var badges = ["bodega", "campCouture","cerifiedHaters", "certifiedW", "cloutCollector","delulu","dripDivison", "emotionallyDestroyed", "girlEra", "glitches", "maleArch", "meme", "performativeMaleUnion", "podiumTalk", "situationshipSociety","slapScale", "snackSnatchers", "swearSquad", "terminallyOnline", "thirstTrapCert", "toxicTalk" ]
+    let columns = [
+        GridItem(.adaptive(minimum: 80))
+    ]
     var body: some View {
         NavigationView {
-            Text("Badge Grid View")
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        NavigateBackView()
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(badges, id: \.self) { badge in
+                        Button {
+                            
+                        } label: {
+                            Image(badge)
+                                .resizable()
+                                .scaledToFit()
+                        }
+                        
                     }
                 }
-        }
+                .padding(.horizontal)
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    NavigateBackView()
+                }
+            }
+        }    .frame(maxHeight: 1000)
+        
     }
 }
 
 #Preview {
     BadgeGridView()
+        .environment(Router())
+    
 }
